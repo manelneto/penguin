7ff500efd04caa5ea622510e823260acec775fe2
Manuel Neto <manuelrlcneto@gmail.com>
2023-10-12 11:02:20 +0100

    added some macros to ll protocol

--- src/link_layer.c
+++ src/link_layer.c
@@ -16,10 +16,14 @@
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
 #define BAUDRATE B38400
+
 #define FLAG 0x7E
 #define A 0x03
 #define C_SET 0x03
+#define C_DISC 0x0B
 #define C_UA 0x07
+#define C_RR(r) 0x05 // | ?
+#define C_REJ(r) 0x01 // | ?
 
 typedef enum {
     START_STATE,
